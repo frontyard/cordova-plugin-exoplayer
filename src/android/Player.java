77beeab3837171010daa8d28bda77d761042edf6
Nedim Cholich <nedim@frontyard.co>
2017-04-27 17:24:31 +0100

    Upgraded ExoPlayer to 2.4.0. Fixed inferContentType.

--- src/android/Player.java
+++ src/android/Player.java
@@ -18,6 +18,7 @@ import com.google.android.exoplayer2.ui.*;
 import com.google.android.exoplayer2.upstream.*;
 import com.google.android.exoplayer2.util.*;
 import com.squareup.picasso.*;
+import java.lang.*;
 import org.apache.cordova.*;
 import org.json.*;
 
@@ -39,6 +40,10 @@ public class Player {
     }
 
     private ExoPlayer.EventListener playerEventListener = new ExoPlayer.EventListener() {
+        @Override
+        public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+            Log.i(TAG, "Playback parameters changed");
+        }
         @Override
         public void onPlayerError(ExoPlaybackException error) {
             Log.e(TAG, "Error in ExoPlayer", error);
@@ -138,8 +143,8 @@ public class Player {
 
     private void preparePlayer(Uri uri) {
         DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
-        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        //TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector();
         LoadControl loadControl = new DefaultLoadControl();
 
         exoPlayer = ExoPlayerFactory.newSimpleInstance(this.activity, trackSelector, loadControl);
@@ -165,30 +170,13 @@ public class Player {
         }
     }
 
-    private static int inferContentType(String fileName) {
-        fileName = fileName.toLowerCase();
-        if (fileName.endsWith(".mpd")) {
-            return C.TYPE_DASH;
-        }
-        else if (fileName.indexOf(".m3u8") > -1) {
-            return C.TYPE_HLS;
-        }
-        else if (fileName.endsWith(".ism") || fileName.endsWith(".isml")
-                || fileName.endsWith(".ism/manifest") || fileName.endsWith(".isml/manifest")) {
-            return C.TYPE_SS;
-        }
-        else {
-            return C.TYPE_OTHER;
-        }
-    }
-
     private MediaSource getMediaSource(Uri uri, DefaultBandwidthMeter bandwidthMeter) {
         String userAgent = Util.getUserAgent(this.activity, config.getUserAgent());
         Handler mainHandler = new Handler();
         HttpDataSource.Factory httpDataSourceFactory = new DefaultHttpDataSourceFactory(userAgent, bandwidthMeter);
         DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this.activity, bandwidthMeter, httpDataSourceFactory);
 
-        int type = inferContentType(uri.toString());
+        int type = Util.inferContentType(uri);
         switch (type) {
             case C.TYPE_DASH:
                 DefaultDashChunkSource.Factory dashChunkSourceFactory = new DefaultDashChunkSource.Factory(dataSourceFactory);
